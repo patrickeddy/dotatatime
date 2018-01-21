43db7da4a1fc2e116ab0061f8a1f23c4dd9aca85
patrickeddy <p@patrickeddy.com>
2018-01-21 12:15:55 -0800

    Initial commit.

---
+++ Assets/Scripts/DotController.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using System.Collections;
+
+public class DotController : MonoBehaviour {
+
+    public DotController () {
+		
+    }
+
+    void Start () {
+		
+    }
+
+    void Update() {
+		
+    }
+
+}
